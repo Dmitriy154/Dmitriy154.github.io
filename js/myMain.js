0f810232e7c5fba760e29c5225eb0b7008b9f7c8
Dmitriy154 <Dmitriy154@gmail.com>
2020-08-07 13:41:43 +0300

    first commit

---
+++ js/myMain.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//Initialization  
+M.AutoInit();
+
+/*
+form_submit.addEventListener('submit', submit_click);
+
+async function submit_click () {
+	
+	let zakaz = {
+		fio: i_fio.value,
+		tel: i_phone.value,
+		comm: textarea1.value
+	};
+	
+	//"text": `Ф.И.О. - ${zakaz.fio}; телефон - ${zakaz.tel}.`
+	
+	let zapros = {
+		"chat_id": "@teplopotok",
+		"photo": "https://sysadmin.pm/wp-content/uploads/2017/07/BotFather.png"
+	}
+
+	
+	//let zapros_json = JSON.stringify(zapros); //JSON.parse - обратно в объект
+
+	let response = await fetch('https://api.telegram.org/bot909184349:AAH0_OkFhIdP7elO7F94TeorS_aAKALdb5Q/sendMessage', {
+	  method: 'POST',
+	  body: {
+		"chat_id": "@teplopotok",
+		"photo": "https://sysadmin.pm/wp-content/uploads/2017/07/BotFather.png"
+	  }
+	});
+
+	let result = await response.json();
+	console.log(result);
+	
+}
+*/
